0d4b2a53255c572e7c5b539e3dd844285581d2e0
Turupawn <ahmed.hn.43@gmail.com>
2015-07-31 14:11:01 -0600

    final de la clase

--- main.cpp
+++ main.cpp
@@ -2,6 +2,10 @@
 #include<SDL2/SDL.h>
 #include<SDL2/SDL_image.h>
 #include<iostream>
+#include<map>
+#include<vector>
+
+using namespace std;
 
 SDL_Window* window;
 SDL_Renderer* renderer;
@@ -41,6 +45,21 @@ int main( int argc, char* args[] )
     SDL_QueryTexture(character, NULL, NULL, &w, &h);
     rect_character.x = 0; rect_character.y = 100; rect_character.w = w; rect_character.h = h;
 
+    char orientation = 'd';// d u l r
+    int current_sprite = 0;
+    int animation_velocity = 20;
+    int velocity = 3;
+    int frame = 0;
+    map<char,vector<SDL_Texture*> >sprites;
+    sprites['u'].push_back(IMG_LoadTexture(renderer, "personaje/up1.png"));
+    sprites['u'].push_back(IMG_LoadTexture(renderer, "personaje/up2.png"));
+    sprites['d'].push_back(IMG_LoadTexture(renderer, "personaje/down1.png"));
+    sprites['d'].push_back(IMG_LoadTexture(renderer, "personaje/down2.png"));
+    sprites['l'].push_back(IMG_LoadTexture(renderer, "personaje/left1.png"));
+    sprites['l'].push_back(IMG_LoadTexture(renderer, "personaje/left2.png"));
+    sprites['r'].push_back(IMG_LoadTexture(renderer, "personaje/right1.png"));
+    sprites['r'].push_back(IMG_LoadTexture(renderer, "personaje/right2.png"));
+
     //Main Loop
     while(true)
     {
@@ -50,16 +69,55 @@ int main( int argc, char* args[] )
             {
                 return 0;
             }
-            if(Event.type == SDL_KEYDOWN)
-            {
-                if(Event.key.keysym.sym == SDLK_d)
-                    rect_character.x++;
-            }
         }
 
+        const Uint8* currentKeyStates = SDL_GetKeyboardState( NULL );
+
+        if(currentKeyStates[ SDL_SCANCODE_D ])
+        {
+            rect_character.x+=velocity;
+            orientation='r';
+        }
+        if(currentKeyStates[ SDL_SCANCODE_A ])
+        {
+            rect_character.x-=velocity;
+            orientation='l';
+        }
+        if(currentKeyStates[ SDL_SCANCODE_S ])
+        {
+            rect_character.y+=velocity;
+            orientation='d';
+        }
+        if(currentKeyStates[ SDL_SCANCODE_W ])
+        {
+            rect_character.y-=velocity;
+            orientation='u';
+        }
+        if(currentKeyStates[ SDL_SCANCODE_LSHIFT ])
+        {
+            velocity=6;
+            animation_velocity=10;
+        }else
+        {
+            velocity=3;
+            animation_velocity=20;
+        }
+
+        if(frame%animation_velocity==0)
+        {
+            current_sprite++;
+            if(current_sprite>1)
+                current_sprite=0;
+        }
+
+        SDL_Delay(17);
+
+        cout<<frame<<endl;
+
         SDL_RenderCopy(renderer, background, NULL, &rect_background);
-        SDL_RenderCopy(renderer, character, NULL, &rect_character);
+        SDL_RenderCopy(renderer, sprites[orientation][current_sprite], NULL, &rect_character);
         SDL_RenderPresent(renderer);
+        frame++;
     }
 
 	return 0;
