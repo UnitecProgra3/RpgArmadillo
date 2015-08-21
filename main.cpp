2c974c9d40368d0872d0b820b77849cd7bd50ff9
Turupawn <ahmed.hn.43@gmail.com>
2015-08-21 13:32:53 -0600

    varios mapas

--- main.cpp
+++ main.cpp
@@ -15,6 +15,22 @@ SDL_Event Event;
 SDL_Texture *background, *texture_npc, *texture_tile;
 SDL_Rect rect_background, rect_character, rect_npc, rect_tile, rect_tileset;
 
+class Warp
+{
+public:
+    int x;
+    int y;
+    string mapa;
+    SDL_Rect rect;
+    Warp(int x,int y,string mapa,SDL_Rect rect)
+    {
+        this->x=x;
+        this->y=y;
+        this->mapa=mapa;
+        this->rect = rect;
+    }
+};
+
 bool collision(SDL_Rect r1, SDL_Rect r2)
 {
     if(r1.x+r1.w <= r2.x)
@@ -29,10 +45,66 @@ bool collision(SDL_Rect r1, SDL_Rect r2)
     return true;
 }
 
-vector<int> getMapa(int layer)
+vector<Warp*> getWarps(string archivo)
+{
+    TiXmlDocument doc(archivo.c_str());
+    bool loadOkay = doc.LoadFile();
+    TiXmlElement *map_node = doc.FirstChild("map")->ToElement();
+    TiXmlNode*objectgroup_node = map_node->FirstChild("objectgroup");
+
+    vector<Warp*> respuesta;
+
+    if(objectgroup_node==NULL)
+        return respuesta;
+
+    for(TiXmlNode*object_node = objectgroup_node->FirstChild("object");
+        object_node!=NULL;
+        object_node=object_node->NextSibling("object"))
+    {
+        int x,y;
+        string mapa;
+        SDL_Rect rect;
+        rect.x = atoi(object_node->ToElement()->Attribute("x"));
+        rect.y = atoi(object_node->ToElement()->Attribute("y"));
+        rect.w = atoi(object_node->ToElement()->Attribute("width"));
+        rect.h = atoi(object_node->ToElement()->Attribute("height"));
+
+        TiXmlNode*properties = object_node->FirstChild("properties");
+        for(TiXmlNode*property = properties->FirstChild("property");
+            property!=NULL;
+            property=property->NextSibling("property"))
+        {
+            if(strcmp(property->ToElement()->Attribute("name"),"x")==0)
+            {
+                cout<<"X:"<<property->ToElement()->Attribute("value")<<endl;
+                x=atoi(property->ToElement()->Attribute("value"));
+            }
+            if(strcmp(property->ToElement()->Attribute("name"),"y")==0)
+            {
+                y=atoi(property->ToElement()->Attribute("value"));
+            }
+            if(strcmp(property->ToElement()->Attribute("name"),"mapa")==0)
+            {
+                mapa=property->ToElement()->Attribute("value");
+            }
+        }
+        Warp*warp = new Warp(x,y,mapa,rect);
+        cout<<warp->x<<endl;
+        cout<<warp->y<<endl;
+        cout<<warp->mapa<<endl;
+        cout<<warp->rect.x<<endl;
+        cout<<warp->rect.y<<endl;
+        cout<<warp->rect.w<<endl;
+        cout<<warp->rect.h<<endl;
+        respuesta.push_back(warp);
+    }
+
+    return respuesta;
+}
+
+vector<int> getMapa(string archivo,int layer)
 {
     vector<int> mapa;
-    string archivo = "tile/test.tmx";
     TiXmlDocument doc(archivo.c_str());
     bool loadOkay = doc.LoadFile();
     TiXmlElement *map_node = doc.FirstChild("map")->ToElement();
@@ -95,6 +167,11 @@ void dibujarLayer(SDL_Renderer* renderer,vector<int>mapa)
 
 bool collisionLayer(vector<int>collision_map,SDL_Rect rect_personaje)
 {
+//    rect_personaje.x+=1;
+//    rect_personaje.y+=1;
+//    rect_personaje.w-=2;
+//    rect_personaje.h-=2;
+
     int x_pantalla = 0;
     int y_pantalla = 0;
     for(int i=0;i<collision_map.size();i++)
@@ -129,11 +206,6 @@ bool collisionLayer(vector<int>collision_map,SDL_Rect rect_personaje)
         {
             if(collision(rect_personaje,rect_tileset))
             {
-                cout<<"Test"<<endl;
-                cout<<rect_personaje.x<<","<<rect_personaje.y<<endl;
-                cout<<rect_personaje.w<<","<<rect_personaje.h<<endl;
-                cout<<rect_tileset.x<<","<<rect_tileset.y<<endl;
-                cout<<rect_tileset.w<<","<<rect_tileset.h<<endl;
                 return true;
             }
         }
@@ -190,8 +262,8 @@ int main( int argc, char* args[] )
     sprites['r'].push_back(IMG_LoadTexture(renderer, "personaje/right2.png"));
 
     SDL_QueryTexture(sprites['u'][0], NULL, NULL, &w, &h);
-    rect_character.x = 0;
-    rect_character.y = 0;
+    rect_character.x = 100;
+    rect_character.y = 100;
     rect_character.w = w;
     rect_character.h = h;
 
@@ -214,9 +286,22 @@ int main( int argc, char* args[] )
     rect_tileset.w = w;
     rect_tileset.h = h;
 
-    vector<int> mapa = getMapa(1);
-    vector<int> mapa2 = getMapa(2);
-    vector<int> collision_map = getMapa(3);
+    map<string,vector<int> >mapas_down;
+    map<string,vector<int> >mapas_over;
+    map<string,vector<int> >mapas_collision;
+    map<string,vector<Warp*> >warps;
+
+    mapas_down["mapa1"]=getMapa("tile/test.tmx",1);
+    mapas_over["mapa1"]=getMapa("tile/test.tmx",2);
+    mapas_collision["mapa1"]=getMapa("tile/test.tmx",3);
+    warps["mapa1"] = getWarps("tile/test.tmx");
+
+    mapas_down["mapa2"]=getMapa("tile/test2.tmx",1);
+    mapas_over["mapa2"]=getMapa("tile/test2.tmx",2);
+    mapas_collision["mapa2"]=getMapa("tile/test2.tmx",3);
+    warps["mapa2"] = getWarps("tile/test2.tmx");
+
+    string mapa_actual="mapa1";
 
     //Main Loop
     while(true)
@@ -234,52 +319,53 @@ int main( int argc, char* args[] )
         if(currentKeyStates[ SDL_SCANCODE_D ])
         {
             rect_character.x+=velocity;
-            if(collision(rect_character,rect_npc))
+            while(collision(rect_character,rect_npc))
             {
-                rect_character.x-=velocity;
+                rect_character.x-=1;
             }
-            if(collisionLayer(collision_map,rect_character))
+            while(collisionLayer(mapas_collision[mapa_actual],rect_character))
             {
-                rect_character.x-=velocity;
+                rect_character.x-=1;
             }
             orientation='r';
         }
         if(currentKeyStates[ SDL_SCANCODE_A ])
         {
             rect_character.x-=velocity;
-            if(collision(rect_character,rect_npc))
+            while(collision(rect_character,rect_npc))
             {
-                rect_character.x+=velocity;
+                rect_character.x+=1;
             }
-            if(collisionLayer(collision_map,rect_character))
+            while(collisionLayer(mapas_collision[mapa_actual],rect_character))
             {
-                rect_character.x+=velocity;
+                rect_character.x+=1;
             }
             orientation='l';
         }
         if(currentKeyStates[ SDL_SCANCODE_S ])
         {
             rect_character.y+=velocity;
-            if(collision(rect_character,rect_npc))
+            while(collision(rect_character,rect_npc))
             {
-                rect_character.y-=velocity;
+                rect_character.y-=1;
             }
-            if(collisionLayer(collision_map,rect_character))
+
+            while(collisionLayer(mapas_collision[mapa_actual],rect_character))
             {
-                rect_character.y-=velocity;
+                rect_character.y-=1;
             }
             orientation='d';
         }
         if(currentKeyStates[ SDL_SCANCODE_W ])
         {
             rect_character.y-=velocity;
-            if(collision(rect_character,rect_npc))
+            while(collision(rect_character,rect_npc))
             {
-                rect_character.y+=velocity;
+                rect_character.y+=1;
             }
-            if(collisionLayer(collision_map,rect_character))
+            while(collisionLayer(mapas_collision[mapa_actual],rect_character))
             {
-                rect_character.y+=velocity;
+                rect_character.y+=1;
             }
             orientation='u';
         }
@@ -300,17 +386,27 @@ int main( int argc, char* args[] )
                 current_sprite=0;
         }
 
+        for(int i=0;i<warps[mapa_actual].size();i++)
+        {
+            if(collision(warps[mapa_actual][i]->rect,rect_character))
+            {
+                rect_character.x=warps[mapa_actual][i]->x;
+                rect_character.y=warps[mapa_actual][i]->y;
+                mapa_actual=warps[mapa_actual][i]->mapa;
+            }
+        }
+
         SDL_Delay(17);
 
         SDL_RenderCopy(renderer, background, NULL, &rect_background);
 
-        dibujarLayer(renderer,mapa);
+        dibujarLayer(renderer,mapas_down[mapa_actual]);
 
         SDL_RenderCopy(renderer, sprites[orientation][current_sprite], NULL, &rect_character);
 
-        dibujarLayer(renderer,mapa2);
+        dibujarLayer(renderer,mapas_over[mapa_actual]);
 
-        dibujarLayer(renderer,collision_map);
+        //dibujarLayer(renderer,collision_map);
 
         SDL_RenderCopy(renderer, texture_npc, NULL, &rect_npc);
         SDL_RenderPresent(renderer);
